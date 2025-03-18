05ae02207b3a81a111111ba39f7cf71521f0f1ed
RichardTao3014 <xtao10@ucsc.edu>
2025-03-18 03:59:41 -0700

    Update Enemy.js

--- src/prefabs/Enemy.js
+++ src/prefabs/Enemy.js
@@ -8,21 +8,6 @@ class Enemy extends Phaser.GameObjects.Sprite {
        // this.moveSpeed = game.settings.spaceshipSpeed         // pixels per frame
     }
 
-    // update() {
-    //     this.x -= this.moveSpeed;
-    //     if(this.x <= 0 - this.width) {
-    //         this.x = game.config.width
-    //     }
-    // }
-
-    // // position reset
-    // reset() {
-    //     // this.x = game.config.width
-    //     // 让 X 和 Y 坐标随机化
-    // this.x = Phaser.Math.Between(game.config.width / 2, game.config.width);  // 右侧随机 X 坐标
-    // this.y = Phaser.Math.Between(borderUISize, game.config.height - borderUISize);  // 随机 Y 坐标
-    // }
-
     update() {
         this.x -= this.moveSpeed;
         if(this.x <= 0 - this.width) {
@@ -30,18 +15,8 @@ class Enemy extends Phaser.GameObjects.Sprite {
         }
     }
 
-    // position reset
-    // reset() {
-    //     // 让 X 和 Y 坐标随机化，使其与玩家范围一致
-    //     this.x = Phaser.Math.Between(borderUISize + this.width, game.config.width - borderUISize - this.width);
-    //     this.y = Phaser.Math.Between(borderUISize + this.height, game.config.height - borderUISize - this.height);
-    // }
-
     reset() {
-        // ✅ X 轴固定在屏幕最右侧
         this.x = game.config.width;
-    
-        // ✅ Y 轴在玩家可移动范围内随机
         this.y = Phaser.Math.Between(
             borderUISize + this.height, 
             game.config.height - borderUISize - this.height
